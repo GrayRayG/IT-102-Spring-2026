fb3737979a7707f44f0d1e22a5f0afe6b3bd1989
Ray G Peckham <raygpeckham@gmail.com>
2026-04-18 22:22:41 -0700

    developing my helloworld

---
+++ start/CH01/helloworld.py
@@ -0,0 +1,13 @@
+'''
+This is just a script to prompt user for user name, 
+then print hello and a positive affirmation.
+'''
+
+#Prompt the user
+guest_name = input("What is your name: ")
+
+#This is to say hello
+print("Hello, " , guest_name)
+
+#Print a postive affirmation
+print("Today is going to be a great day. ")
