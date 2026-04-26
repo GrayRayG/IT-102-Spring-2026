aa8313d21b0f07a9d847be2fed7523967dac9fc6
Ray G Peckham <raygpeckham@gmail.com>
2026-04-25 18:29:59 -0700

    created a new conditonal python script

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,11 @@
-#!/usr/bin/env python3
-# example workign with conditionals
-#By 
+'''
+Create a conditional that takes an input and if its yes then return a reply
+'''
+
+#This is going to take an input from the user
+answer = input("Is today a good day? (y/n) ").lower()
+
+
+#it is an if statement checking if the string is equal to y and if so print yes it is
+if answer == "y":
+    print("Yes it is")
