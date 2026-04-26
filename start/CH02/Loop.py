d417be5635f7455cfc84e1177a03af8c27c3e242
Ray G Peckham <raygpeckham@gmail.com>
2026-04-25 19:04:21 -0700

    created a for loop on the answer statement

---
+++ start/CH02/Loop.py
@@ -0,0 +1,16 @@
+'''
+Create a loop and conditional that takes an input and if its yes print it 10 times then return a reply
+'''
+
+#This is going to take an input from the user
+answer = input("Is today a good day? (y/n) ").lower()
+
+
+#it is an if statement checking if the string is equal to y and if so print yes it is
+if answer == "y":
+    for _ in range(10):
+        print("Yes it is")
+elif answer == "n":
+    print("I'm sorry")
+else:
+    print("Please try again")
