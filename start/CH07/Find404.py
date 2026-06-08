091117479dd0fcd485f4060660ab59cf20856961
Ray G Peckham <raygpeckham@gmail.com>
2026-06-07 22:32:35 -0700

    created a 404 and find status code

--- start/CH07/Find404.py
+++ start/CH07/Find404.py
@@ -1,3 +1,24 @@
-#!/usr/bin/env python3
-# Script that scans web server logs for 404 errors
-# By 
+#print any IP that gets a 404 status code
+
+"""
+The purpose of this script is to return 404 source IP if its found
+"""
+
+
+#libraries utilized
+import re
+
+def main():
+    LOG_FILE = r"/Users/raygpeckham/IT-102-Spring-2026/start/CH07/access.log"
+    with open(LOG_FILE, "r") as f:
+        for line in f:
+            #status code which is right after request and first nuymbers
+            ip = line.split()[0]
+            match = re.search(r'" 404 ', line)
+            if match:
+                print(f"{ip}")
+
+
+if __name__ == "__main__":
+    main()
+
